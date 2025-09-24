109f097d33dbf885bdccbd563c68f81d53b73176
MarcSau <Marc-Work@DESKTOP-1L66BP0>
2025-09-24 12:46:26 +0200

    [Update] Fixed issues with endpoint and mapping

--- StoreBackend.Api/Endpoints/ProductEndpoints.cs
+++ StoreBackend.Api/Endpoints/ProductEndpoints.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using StoreBackend.Api.Data;
 using StoreBackend.Api.DTOs;
+using StoreBackend.Api.Entities;
 using StoreBackend.Api.Mapping;
 namespace StoreBackend.Api.Endpoints;
 
@@ -16,9 +17,10 @@ public static class ProductEndpoints
 
         });
 
-        group.MapGet("/", (int id, StoreContext dbContext) =>
+        group.MapGet("/{id}", (int id, StoreContext dbContext) =>
         {
-            var product = dbContext.Products.Find(id);
+            var product = dbContext.Products.Include(product => product.ProductType).Where(product => product.ID == id).FirstOrDefault();
+            
             if (product == null)
             {
                 return Results.NotFound();
--- StoreBackend.Api/obj/Debug/net9.0/StoreBackend.Api.AssemblyInfo.cs
+++ StoreBackend.Api/obj/Debug/net9.0/StoreBackend.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreBackend.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e48a85b9f8aadac5f08b33004aa09e61b96f1d03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69ad56ca672f6b03703b8bc856e459fec6c7542")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreBackend.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreBackend.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
